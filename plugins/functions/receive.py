fc3df5d16ce7596b6adab504a798078ab4a1a6b7
Xiao <xiao@scp-079.org>
2019-10-18 11:02:39 +0000

    - adjust code style

--- plugins/functions/receive.py
+++ plugins/functions/receive.py
@@ -18,6 +18,7 @@
 
 import logging
 import pickle
+from copy import deepcopy
 from json import loads
 from typing import Any
 
@@ -259,6 +260,8 @@ def receive_remove_bad(data: dict) -> bool:
 
         if the_type == "user":
             glovar.bad_ids["users"].discard(the_id)
+            glovar.user_ids[the_id] = deepcopy(glovar.default_user_status)
+            save("user_ids")
 
         save("bad_ids")
 
